9db7f5d29278e3eb971126e46ee372fd0acff47f
Betsimisaraka <anita.ras@onja.org>
2022-05-11 17:31:40 +0300

    Start the header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@ module.exports = {
   content: ["./**/*.html"],
   theme: {
     extend: {},
+    colors: {
+      'white': '#ffffff',
+      'black': '#000000',
+      'mimosa': '#F1FFBC',
+      'smoke': '#3A3A4E',
+      'dark-gray': '#7E7E81',
+    },
+    
+    fontFamily: {
+      'sailec': ['Sailec-Bold, sans-serif'],
+      'tiempos': ['Tiempos Headline, sans-serif'],
+    },
   },
   plugins: [],
 }
