cb9f378b7765cb3160c3237f352db9ff42cf5578
John Evans <john@wearelighthouse.com>
2022-05-25 18:54:42 +0100

    --wip-- Next event section

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
       'white': '#ffffff',
       'black': '#000000',
       'mimosa': '#F1FFBC',
+      'pink': '#E80070',
       'smoke': '#3A3A4E',
       'dark-gray': '#7E7E81',
     },
