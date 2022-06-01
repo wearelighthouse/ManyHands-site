dac4427b838fa95611c9a646ab1dc4ff1bd06e51
John Evans <john@wearelighthouse.com>
2022-06-01 14:03:42 +0100

    Clutch logo, misc brought-to-you-by style & markup changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,14 +29,15 @@ module.exports = {
     },
 
     fontSize: {
+      'md': '1rem',
       'lg': '1.125rem',
       'xl': '1.25rem',
       '2xl': '1.5rem',
       '3xl': '1.75rem',
-      '3.5xl': '2rem',
-      '4xl': '2.125rem',
-      '5xl': '3rem',
-      '6xl': '3.75rem',
+      '3.5xl': '2rem', // 32px
+      '4xl': '2.125rem', // 34px
+      '5xl': '3rem', // 48px
+      '6xl': '3.75rem', // 60px
     },
 
     screens: {
